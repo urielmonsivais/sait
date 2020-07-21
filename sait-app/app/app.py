b894a47aa9687feb37dc1cfcc8fe54a0b7df5fd1
urielmonsivais <55606914+urielmonsivais@users.noreply.github.com>
2020-07-21 15:29:59 -0500

    subiendo cambios

--- sait-app/app/app.py
+++ sait-app/app/app.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
