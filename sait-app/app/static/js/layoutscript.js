850400f1bd3f733c093ddc2f4403ac04c7547dd6
kiikedls <jespinozadelossatnos@gmail.com>
2020-08-07 21:05:34 -0500

    cree un directorio para colocar mis scripts personalizados y agrege un popper al boton de logout

---
+++ sait-app/app/static/js/layoutscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+$(function () {
+  $('#logout').popover({
+    placement:"left",
+    trigger:"hover"
+  })
+});
+$(function () {
+  $('#pop').popover({
+    placement:"top",
+    trigger:"hover"
+  })
+});
