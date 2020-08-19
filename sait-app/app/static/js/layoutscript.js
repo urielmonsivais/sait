336f2033cd80c20c823c712aaa189eafee015024
kiikedls <jespinozadelossatnos@gmail.com>
2020-08-18 19:55:58 -0500

    cambios de diseño y agregue un codigo js para la plantilla

--- sait-app/app/static/js/layoutscript.js
+++ sait-app/app/static/js/layoutscript.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-            });
-        });
-$(function () {
-  $('#logout').popover({
-    placement:"left",
-    trigger:"hover"
-  })
+$(document).ready(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
 });
-$(function () {
-  $('#pop').popover({
-    placement:"top",
-    trigger:"hover"
-  })
+$(function() {
+    $('#logout').popover({
+        placement: "left",
+        trigger: "hover"
+    })
+});
+$(function() {
+    $('#pop').popover({
+        placement: "top",
+        trigger: "hover"
+    })
 });
