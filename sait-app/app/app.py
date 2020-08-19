4da6499ff9614d6af235dc6dbd55dc9e1d53bed5
El nuevo usuario <viboni_18@hotmail.com>
2020-08-18 21:04:16 -0500

    Consulta de registro y traer registros de la base de datos

--- sait-app/app/app.py
+++ sait-app/app/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost' 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'farmacia'
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -17,4 +23,16 @@ def welcome():
 
 @app.route('/prueba')
 def prueba():
-    return render_template('prueba.html')
+	cur = mysql.connection.cursor()
+	cur.execute("INSERT INTO acciones (id,nombre,direccion) VALUES (%s,%s,%s)", (None, 'Test Action', 'Test Address'))
+	mysql.connection.commit()
+	return render_template('prueba.html')
+
+@app.route('/Dashboard')
+def Dashboard():
+	cur = mysql.connection.cursor()
+	cur.execute("SELECT * FROM acciones")
+	actions = cur.fetchall()
+	cur.close()	
+	print(actions)
+	return render_template('Dashboard.html', actions = actions)
