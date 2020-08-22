5e2dff4dacef57cc9d2c268debcc88f6284b0f92
kiikedls <jespinozadelossatnos@gmail.com>
2020-08-21 23:18:26 -0500

    arreglado mi problema con la conexion SQL y agregamos los modulos para que trabaje el el email

--- sait-app/app/app.py
+++ sait-app/app/app.py
@@ -1,17 +1,55 @@
-from flask import Flask, render_template
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request
+from flask_mail import Mail
+from flask_mail import Message
+# from flask_mysqldb import MySQL <---los demas equipos
+from flaskext.mysql import MySQL #este es de kiike 
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost' 
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'sait_db'
+app.config['MYSQL_DB'] = 'farmacia'
 mysql = MySQL(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'monsivaisuriel28@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hpqgwjliswijqrwb'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# para el mail
+@app.route('/contact', methods=['POST'])
+def contact():
+    request_c = request.form
+    try:
+        msg = Message("New request",
+                  sender="monsivaisuriel28@gmail.com",
+                  recipients=["uriel.monsivais@sait.red","enrique.espinoza@sait.red"])
+        mailBody = """ 
+        Hola, nuevo correo de {0}.
+
+        Datos:
+
+            Nombre: {1}
+            Telefono: {2}
+            Numéro de empleados: {3}
+            Servicios de interes: {4} 
+        
+        """.format(request_c['mail'],request_c['name'],request_c['phone'],request_c['employees'],request_c['services'])
+        msg.body = mailBody
+        mail.send(msg)
+    except ValueError:
+        print("Email error")
+        print(ValueError)
+    return render_template('index.html')
+#aqui termina el mail
 
 @app.route('/signin')
 def signin():
@@ -30,8 +68,11 @@ def prueba():
 
 @app.route('/Dashboard')
 def Dashboard():
-	cur = mysql.connection.cursor()
-	cur.execute("SELECT activos.nombre , periodo_pago.f_termino, periodo_pago.f_corte , periodo_pago.f_pago FROM (periodo_pago LEFT JOIN activos  ON periodo_pago.id = activos.id) ")
+	# cur = mysql.connection.cursor()
+	# aqui empece
+	conn = mysql.connect()
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM farmacia.acciones")
 	actions = cur.fetchall()
 	cur.close()	
 	print(actions)
